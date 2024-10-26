c2146cdc7e332fe4fbd9e20b2c096e01f876dfb5
Steven Calderon <steven.calderon@sofka.com.co>
2024-10-26 10:48:07 -0500

    Inicio de proyecto

---
+++ src/test/java/co/edu/usbcali/gestionrh/GestionRhApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.gestionrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
