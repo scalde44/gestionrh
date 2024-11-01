31755840b7c1df18d71f9ef4684da1fc97c3e020
Steven Calderon <steven.calderon@sofka.com.co>
2024-11-01 13:09:51 -0500

    Se mueven de paquetes las clases

--- src/main/java/co/edu/usbcali/gestionrh/domain/Empleado.java
+++
@@ -1,4 +0,0 @@
-package co.edu.usbcali.gestionrh.domain;
-
-public class Empleado {
-}
--- src/main/java/co/edu/usbcali/gestionrh/model/domain/Cargo.java
+++ src/main/java/co/edu/usbcali/gestionrh/model/domain/Cargo.java
@@ -1,15 +1,20 @@
-package co.edu.usbcali.gestionrh.domain;
+package co.edu.usbcali.gestionrh.model.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,10 +24,13 @@ import lombok.NoArgsConstructor;
 @Table(name = "cargos")
 public class Cargo {
     @Id
-    @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
-    
+
     @Column(nullable = false, length = 50)
     private String nombre;
+
+    @OneToMany(mappedBy = "cargo", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ContratoEmpleado> contratosEmpleados;
 }
